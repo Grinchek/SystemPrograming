804909dfe1a215b9cbb8ae224a67f9196aa955eb
Grinchik <warning686@gmail.com>
2024-10-10 11:46:18 +0300

    async await homework

--- SystemPrograming/05_async_await/MainWindow.xaml.cs
+++ SystemPrograming/05_async_await/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,11 +10,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using MessageBox = System.Windows.Forms.MessageBox;
+//using System.Windows.Shapes;
 
 namespace _05_async_await
 {
@@ -22,6 +26,8 @@ namespace _05_async_await
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string source = "";
+        static string destination = "";
         static Random random = new Random();
         public MainWindow()
         {
@@ -29,45 +35,6 @@ namespace _05_async_await
         }
         //async - allow method to use await keyword
         //await - wait task without freezing
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //int value = GenerateValue();//freeze
-
-            //Task<int> task = Task.Run(GenerateValue);
-
-            //task.Wait();//freeze
-            // list.Items.Add(task.Result);  //freeze
-
-            //int value = await task;
-            //int value = await Task.Run(GenerateValue);
-            //int value = await GenerateValueAsync();
-            //MessageBox.Show("Complete in button click!!!!");
-            //list.Items.Add(value);  
-            // GenerateValueAsync();
-            //await GenerateValueAsync();
-            //await GenerateValueAsync();
-            //await GenerateValueAsync();
-
-            list.Items.Add(await GenerateValueAsync());
-
-        }
-
-        int GenerateValue()
-        {
-            Thread.Sleep(random.Next(5000));
-            //MessageBox.Show("Complete!!!!");
-            return random.Next(1000);
-        }
-        Task<int> GenerateValueAsync()
-        {
-            return Task.Run(() =>
-            {
-                Thread.Sleep(random.Next(5000));
-                //MessageBox.Show("Complete!!!!");
-                return random.Next(1000);
-            });
-
-        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -78,7 +45,7 @@ namespace _05_async_await
 
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-
+                source = dialog.FileName;
                 MessageBox.Show("You selected: " + dialog.FileName);
             }
         }
@@ -92,9 +59,22 @@ namespace _05_async_await
 
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-
+                destination = dialog.FileName;
                 MessageBox.Show("You selected: " + dialog.FileName);
             }
+
+        }
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            File.Copy(source, destination + "\\copy" + Path.GetFileName(source));
+            MessageBox.Show($"Complited");
+        }
+        private async void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            source = from.Text;
+            destination = to.Text;
+            File.Copy(source, destination + "\\copy" + Path.GetFileName(source));
+            MessageBox.Show($"Complited");
         }
     }
 }
